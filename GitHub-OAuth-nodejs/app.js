e85704846635fac4c5e998f9cca3c35db65cf793
samar abdelhmeed <samar.abdelhmeed77@gmail.com>
2023-10-03 16:33:32 +0300

    add vercel.json file

--- GitHub-OAuth-nodejs/app.js
+++ GitHub-OAuth-nodejs/app.js
@@ -14,14 +14,37 @@ app.get("/auth", (req, res) => {
 
 app.get("/callback", async (req, res) => {
     try {
-        // ... (rest of your code for handling OAuth)
+        const code = req.query.code;
+        const clientId = "da7ff8b08525ef4711d2";
+        const clientSecret = "184a03f0e529a5bdb1b7ba92db1771d6b312f2c0";
+
+        const result = await axios.post("https://github.com/login/oauth/access_token", {
+            client_id: clientId,
+            client_secret: clientSecret,
+            code: code
+        }, {
+            headers: {
+                Accept: "application/json"
+            }
+        });
+
+        const accessToken = result.data.access_token;
+
+        const response = await axios.get("https://api.github.com/user", {
+            headers: {
+                Authorization: `token ${accessToken}`
+            }
+        });
+
+        const userData = response.data;
+        res.send(`Welcome, ${userData.login}!`);
     } catch (err) {
         console.error("Error:", err.message);
         res.status(500).send("Internal Server Error");
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
