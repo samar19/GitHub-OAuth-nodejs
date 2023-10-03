904d337245013e1b478d79ea4fa3e38fbfbdfa98
samar abdelhmeed <samar.abdelhmeed77@gmail.com>
2023-10-03 14:34:25 +0300

    vercel4

--- GitHub-OAuth-nodejs/app.js
+++ GitHub-OAuth-nodejs/app.js
@@ -12,36 +12,36 @@ app.get("/auth", (req, res) => {
     res.redirect(`https://github.com/login/oauth/authorize?client_id=da7ff8b08525ef4711d2`);
 });
 
-app.get("/callback", (req, res) => {
-    const code = req.query.code;
-    const clientId = "da7ff8b08525ef4711d2";
-    const clientSecret = "184a03f0e529a5bdb1b7ba92db1771d6b312f2c0";
+app.get("/callback", async (req, res) => {
+    try {
+        const code = req.query.code;
+        const clientId = "da7ff8b08525ef4711d2";
+        const clientSecret = "184a03f0e529a5bdb1b7ba92db1771d6b312f2c0";
 
-    axios.post("https://github.com/login/oauth/access_token", {
-        client_id: clientId,
-        client_secret: clientSecret,
-        code: code
-    }, {
-        headers: {
-            Accept: "application/json"
-        }
-    }).then((result) => {
+        const result = await axios.post("https://github.com/login/oauth/access_token", {
+            client_id: clientId,
+            client_secret: clientSecret,
+            code: code
+        }, {
+            headers: {
+                Accept: "application/json"
+            }
+        });
+
         const accessToken = result.data.access_token;
-        axios.get("https://api.github.com/user", {
+
+        const response = await axios.get("https://api.github.com/user", {
             headers: {
                 Authorization: `token ${accessToken}`
             }
-        }).then((response) => {
-            const userData = response.data;
-            res.send(`Welcome, ${userData.login}!`);
-        }).catch((err) => {
-            console.error("Error fetching user data from GitHub:", err.response.data);
-            res.status(500).send("Error fetching user data from GitHub");
         });
-    }).catch((err) => {
-        console.error("Error exchanging code for access token:", err.response.data);
-        res.status(500).send("Error exchanging code for access token");
-    });
+
+        const userData = response.data;
+        res.send(`Welcome, ${userData.login}!`);
+    } catch (err) {
+        console.error("Error:", err.message);
+        res.status(500).send("Internal Server Error");
+    }
 });
 
 const PORT = process.env.PORT || 8000;
